be8130fdb508c7936704b319debd025aa6f4b833
Akbar <akbarjonjorayev001@gmail.com>
2025-01-07 11:18:58 +0500

    link added

--- src/pages/Home/Try/Try.jsx
+++ src/pages/Home/Try/Try.jsx
@@ -1,14 +1,19 @@
+import { useRef } from 'react'
 import Button from '../../../components/Button/Button.jsx'
 import './Try.css'
 import { Link } from 'react-router-dom'
 import logo from '../../../media/logo/logo.jpg'
 
 export default function Try() {
+  const extensionLink = useRef(
+    'https://chromewebstore.google.com/detail/disliko/lggcieolgjndakkpgmokiomipmmocenn'
+  ).current
+
   return (
     <>
       <div className="home_about list_y d_f_ce">
         <img className="try_img" src={logo} alt="Disliko logo" />
-        <Link className="try_btn" to="/">
+        <Link className="try_btn" to={extensionLink}>
           <Button>Explore</Button>
         </Link>
       </div>
